ff8d751f7b95720f13801f34f1dad4f59ecc39cf
Michael <mdlevick@gmail.com>
2020-01-21 10:15:48 -0600

    api done

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const AccountRouter = require("./accounts/router.js");
+
+// const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("<h3>Monday, December 9th Project.</h3>");
+});
+
+server.use("/api/accounts", AccountRouter);
 
 module.exports = server;
